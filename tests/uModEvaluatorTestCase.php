4d1b074f22e83f725c5ac5992207a5737e981f9a
Calytic <cyclonenetwork2015@gmail.com>
2018-05-19 22:42:25 -0400

    test update

--- tests/uModEvaluatorTestCase.php
+++ tests/uModEvaluatorTestCase.php
@@ -1,21 +1,20 @@
 <?php
-namespace uMod\Evaluator\Tests {
+namespace uMod\Evaluator\Tests;
+/**
+* Base test case class
+* @package uMod/uModEvaluator
+*/
+abstract class uModEvaluatorTestCase extends \PHPUnit_Framework_TestCase {
     /**
-    * Base test case class
-    * @package uMod/uModEvaluator
+    * Perform setUp tasks
     */
-    abstract class uModEvaluatorTestCase extends \PHPUnit_Framework_TestCase {
-        /**
-        * Perform setUp tasks
-        */
-        protected function setUp()
-        {
-        }
-        /**
-         * Perform clean up / tear down tasks
-         */
-        protected function tearDown()
-        {
-        }
+    protected function setUp()
+    {
+    }
+    /**
+     * Perform clean up / tear down tasks
+     */
+    protected function tearDown()
+    {
     }
 }
